5d99b4805eef9f533dc79718cf71ff376bebdbd1
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-05-06 23:14:17 +0100

    🎉
    config init create server com express

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API de E-commerce funcionando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});

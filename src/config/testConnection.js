9a9d37bd817fbd5750acdaf2dfe3d271dfbcaa95
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-05-10 17:18:20 +0100

    🚀
    improve project structure

--- src/config/testConnection.js
+++ src/config/testConnection.js
@@ -4,10 +4,10 @@ import pool from './db.js';
 (async () => {
   try {
     const res = await pool.query('SELECT NOW()');
-    console.log('Conexão bem-sucedida!', res.rows);
+    console.log('✅ Conectado ao banco:', res.rows[0].now);
   } catch (err) {
-    console.error('Erro ao conectar ao banco:', err);
+    console.error('❌ Erro ao conectar ao banco:', err);
   } finally {
-    await pool.end();
+    pool.end();
   }
 })();
